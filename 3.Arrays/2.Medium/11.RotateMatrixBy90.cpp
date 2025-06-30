904f18d364c9a7915ea56042510146a7e9f59154
arindadl1 <arindalchar17@gmail.com>
2025-06-30 22:35:49 +0530

    day 17

---
+++ 3.Arrays/2.Medium/11.RotateMatrixBy90.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void rotateMatrix(vector<vector<int>>& matrix, int n) {
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    rotateMatrix(matrix, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
